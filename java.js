7c8cb02e94a7a7b0b50df9e637d889374a67a482
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-07-24 13:18:53 +0300

    added icon3 toggle

--- java.js
+++ java.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
         $("#icon1t").toggle();
         $("#img1show").toggle()
     })
+    $("#icon3").click(function(){
+        $("#icon3t").toggle();
+        $("#img3show").toggle();
+    })
 })
