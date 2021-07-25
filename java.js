11767764708b37622efc85f5eac70bf43afd5686
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-07-25 23:14:27 +0300

    added hover effects

--- java.js
+++ java.js
@@ -12,6 +12,57 @@ $(document).ready(function(){
         $("#img2show").toggle();
     })
 
-})
+   
+});
+
+$(document).ready(function(){
+    $(".bg1").mouseover(function(){
+      $(".po1").show();
+    }).mouseout(function(){
+      $(".po1").hide();
+    });
+
+    $(".bg2").mouseover(function(){
+        $(".po2").show();
+      }).mouseout(function(){
+        $(".po2").hide();
+      });
+
+      $(".bg3").mouseover(function(){
+        $(".po3").show();
+      }).mouseout(function(){
+        $(".po3").hide();
+      });
+
+      $(".bg4").mouseover(function(){
+        $(".po4").show();
+      }).mouseout(function(){
+        $(".po4").hide();
+      });
+
+      $(".bg5").mouseover(function(){
+        $(".po5").show();
+      }).mouseout(function(){
+        $(".po5").hide();
+      });
+
+      $(".bg6").mouseover(function(){
+        $(".po6").show();
+      }).mouseout(function(){
+        $(".po6").hide();
+      });
+
+      $(".bg7").mouseover(function(){
+        $(".po7").show();
+      }).mouseout(function(){
+        $(".po7").hide();
+      });
+
+      $(".bg8").mouseover(function(){
+        $(".po8").show();
+      }).mouseout(function(){
+        $(".po8").hide();
+      });
+});
 
 
